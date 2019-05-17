cba96d041528174cf89c15b888ad1846e7ae3465
rafa_99 <rafa99@protonmail.com>
2019-05-17 00:17:00 +0000

    Updated Spaces Music Selecting Script

--- .config/custom_scripts/MusicSelect.sh
+++ .config/custom_scripts/MusicSelect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mpc playlist | nl | cut -c 4- | sed 's:\t: :g' | dmenu -i -l 30 | awk '{print $1}' | xargs -I {} mpc play "{}"
+mpd & mpc playlist | nl | cut -c 4- | sed 's:\t:     :g' | dmenu -i -l 30 | awk '{print $1}' | xargs -I {} mpc play "{}"
