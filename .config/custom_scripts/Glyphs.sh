e2b1b903a46292b831a9509d3343e8bbec1d48b3
rafa_99 <rafa99@protonmail.com>
2019-06-02 15:05:11 +0000

    Homonized Scripts

--- .config/custom_scripts/Glyphs.sh
+++ .config/custom_scripts/Glyphs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat ~/.config/glyphsrc | dmenu -i -l 20 | awk '{print $1}' | xargs -I {} printf {} | xclip -selection clipboard
+cat $HOME/.config/glyphsrc | dmenu -i -l 20 | awk '{print $1}' | xargs -I {} printf {} | xclip -selection clipboard
