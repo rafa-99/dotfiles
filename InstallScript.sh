9f26c1ead10cae6c6f92a27dd60fab175bbc382e
rafa_99 <rafa99@protonmail.com>
2019-06-19 22:14:28 +0000

    Updated Deps

--- InstallScript.sh
+++ InstallScript.sh
@@ -6,7 +6,7 @@ echo "######################"
 echo "    for Arch Linux    "
 echo "**********************"
 
-pacman -S base-devel vim ncmpcpp mpd mpc xcompmgr xwallpaper xclip newsboat ranger zathura R qutebrowser tcl tk mpv
+pacman -S base-devel vim ncmpcpp mpd mpc xcompmgr xwallpaper xclip newsboat ranger zathura R qutebrowser tcl tk mpv libpng libjpeg-turbo webkit2gtk gtk2 gtk3 gtkmm gtkmm3 wxgtk-common wxgtk3
 cp -rf .* $HOME
 cp -rf Pictures/* $HOME/Pictures/
 rm -rf $HOME/.git
