a8ec146bd2039b06869e73772f6461ed012b265b
rafa_99 <rafa99@protonmail.com>
2019-06-21 13:26:41 +0000

    Updated mimeapps and Install Script Deps

--- InstallScript.sh
+++ InstallScript.sh
@@ -6,7 +6,7 @@ echo "######################"
 echo "    for Arch Linux    "
 echo "**********************"
 
-pacman -S base-devel vim ncmpcpp mpd mpc xcompmgr xwallpaper xclip newsboat ranger zathura R qutebrowser tcl tk mpv libpng libjpeg-turbo webkit2gtk gtk2 gtk3 gtkmm gtkmm3 wxgtk-common wxgtk3
+pacman -S geany base-devel vim ncmpcpp mpd mpc xcompmgr xwallpaper xclip newsboat ranger zathura R qutebrowser tcl tk mpv libpng libjpeg-turbo webkit2gtk gtk2 gtk3 gtkmm gtkmm3 wxgtk-common wxgtk3 grc gst-libav gst-plugins-good
 cp -rf .* $HOME
 cp -rf Pictures/* $HOME/Pictures/
 rm -rf $HOME/.git
