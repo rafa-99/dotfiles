b386e5c93a48d5e1ac941b6f093ef6a7d7ce98e0
rafa_99 <rafa99@protonmail.com>
2020-02-21 21:15:36 +0000

    Added Red Color Scheme

---
+++ .bootstrap/colors/Red/st/colors.c
@@ -0,0 +1,27 @@
+const char *colorname[] = {
+
+  /* 8 normal colors */
+  [0] = "#0f1014", /* black   */
+  [1] = "#7C274D", /* red     */
+  [2] = "#6D384A", /* green   */
+  [3] = "#A12B3A", /* yellow  */
+  [4] = "#D82938", /* blue    */
+  [5] = "#F6553A", /* magenta */
+  [6] = "#993646", /* cyan    */
+  [7] = "#db9c9a", /* white   */
+
+  /* 8 bright colors */
+  [8]  = "#996d6b",  /* black   */
+  [9]  = "#7C274D",  /* red     */
+  [10] = "#6D384A", /* green   */
+  [11] = "#A12B3A", /* yellow  */
+  [12] = "#D82938", /* blue    */
+  [13] = "#F6553A", /* magenta */
+  [14] = "#993646", /* cyan    */
+  [15] = "#db9c9a", /* white   */
+
+  /* special colors */
+  [256] = "#0f1014", /* background */
+  [257] = "#db9c9a", /* foreground */
+  [258] = "#db9c9a",     /* cursor */
+};
