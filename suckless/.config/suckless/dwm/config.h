b9262aa6852601814f95c408e5220d5fb79e5b44
Rafael Marçalo <raroma09@gmail.com>
2023-08-17 16:34:31 +0100

    Updated compiler and dwm configuration

--- suckless/.config/suckless/dwm/config.h
+++ suckless/.config/suckless/dwm/config.h
@@ -3,7 +3,7 @@
 #include "colors.h"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
