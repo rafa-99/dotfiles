ccad5d428409d4939b8e43cb5ba2f2d6620287e8
Rafael Marcalo <public@rafaelmarcalo.xyz>
2024-11-26 14:30:48 +0000

    Added slock

---
+++ suckless/.config/suckless/slock/config.h
@@ -0,0 +1,17 @@
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius=10;
+/*Enable Pixelation*/
+//#define PIXELATION
+/*Set pixelation radius*/
+static const int pixelSize=0;
